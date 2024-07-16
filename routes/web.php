4618c620fedb773bfd712787d6bfaa401192a1b5
yehnda <brianboss15782@gmail.com>
2024-07-17 02:44:30 +0300

    Feature:Update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +25,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/chat/{user}', [ChatController::class, 'show'])->name('chat');
+
+    Route::get('/messages/{user}', [ChatController::class, 'getMessages']);
+    Route::post('/messages/{user}', [ChatController::class, 'sendMessage']);
 });
 
 require __DIR__.'/auth.php';
